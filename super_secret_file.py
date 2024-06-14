e4e148d415f3539aa286ed99f96a396fc7a41f78
Gary K <33300872+d0ntblink@users.noreply.github.com>
2024-06-14 06:01:44 -0700

    Refactor code to improve performance and readability

---
+++ super_secret_file.py
@@ -0,0 +1,64 @@
+Amazon Web Services (AWS)
+
+Access Key ID: AKIAIOSFODNN7EXAMPLE
+Secret Access Key: wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+Region: us-west-2
+Google Cloud Platform (GCP)
+
+Project ID: my-project-123456
+Private Key ID: abcdef1234567890abcdef1234567890abcdef12
+Private Key:
+
+vbnet
+
+-----BEGIN PRIVATE KEY-----
+MIIEvAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBAL1...
+-----END PRIVATE KEY-----
+
+Client Email: my-service-account@my-project-123456.iam.gserviceaccount.com
+Client ID: 123456789012345678901
+Microsoft Azure
+
+Subscription ID: 12345678-1234-1234-1234-123456789012
+Tenant ID: 12345678-1234-1234-1234-123456789012
+Client ID (Application ID): 12345678-1234-1234-1234-123456789012
+Client Secret: abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890=
+IBM Cloud
+
+API Key: abcdefghijklmnopqrstuvwx-1234567890abcdefghij
+Resource Group ID: abcde1234567890abcdef1234567890
+Oracle Cloud
+
+Tenancy OCID: ocid1.tenancy.oc1..aaaaaaaaxxxxxxx
+User OCID: ocid1.user.oc1..aaaaaaaaxxxxxxx
+Fingerprint: 20:3b:97:13:55:1b:fc:ed:3b:bb:1a:ee:e6:93:64:12
+Private Key:
+
+vbnet
+
+-----BEGIN RSA PRIVATE KEY-----
+MIICXAIBAAKBgQCv+5mW/yL2u6Mf5O7y+5+jfWl...
+-----END RSA PRIVATE KEY-----
+
+DigitalOcean
+
+API Token: abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890
+Heroku
+
+API Key: 12345678-1234-1234-1234-123456789012
+Firebase
+
+API Key: AIzaSyA-abc123def456ghijklm789nopqrstu012v
+Project ID: my-firebase-project-123456
+Messaging Sender ID: 123456789012
+App ID: 1:123456789012:web
+Salesforce
+
+Consumer Key: 3MVG9szVa2RxsqB5dFJsHQoGm0sZR1xFmZkPTt9eKIXzLxgRzNfPx1A8QFjBRH
+Consumer Secret: ABCDEF1234567890abcdef1234567890abcdef1234567890
+Username: myusername@example.com
+Password: mypassword123
+Security Token: abcdef1234567890
+GitHub
+
+Personal Access Token: ghp_1234567890abcdef1234567890abcdef12345678
